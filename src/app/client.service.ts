dcdf0c08f9b67730e2c9810e9588e66556c857d4
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2023-05-16 09:29:06 -0300

    :sparkles: feat: Adiciona o remover o atualizar ao projeto

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -20,4 +20,12 @@ export class ClientService {
   save(client: Client): Observable <Client>{
     return this.http.post<Client>(this.url, client);
   }
+
+  remove(client: Client): Observable <void>{
+    return this.http.delete<void>(this.url + client.id);
+  }
+
+  edit(client: Client): Observable <Client>{
+    return this.http.put<Client>(this.url + client.id, client);
+  }
 }
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -12,6 +12,8 @@ export class ClientsComponent implements OnInit{
 
   clients: Client[]  = [];
   formGroupClient: FormGroup;
+  isEditing: boolean = false;
+  currentClient: Client = {} as Client;
 
   constructor(private clientService: ClientService, private formBuilder: FormBuilder) {
     this.formGroupClient = formBuilder.group({
@@ -37,14 +39,38 @@ export class ClientsComponent implements OnInit{
   }
 
   save(){
-    this.clientService.save(this.formGroupClient.value).subscribe({
-      next: data => {
-        this.clients.push(data);
-        this.formGroupClient.reset();
-        console.log(this.clients);
+    if(this.isEditing){
+      this.clientService.edit(this.formGroupClient.value).subscribe({
+        next: data => {
+          let index = this.clients.indexOf(this.currentClient);
+          this.clients[index] = data;
+        }
+      })
+      this.isEditing = false;
+    }
+    else{
+      this.clientService.save(this.formGroupClient.value).subscribe({
+        next: data => {
+          this.clients.push(data);
+          console.log(this.clients);
+        }
+      })
+    }
+    this.formGroupClient.reset();
+  }
+
+  remove(client: Client){
+    this.clientService.remove(client).subscribe({
+      next: () => {
+        let index = this.clients.indexOf(client);
+        this.clients.splice(index, 1);
       }
-    })
+    });
+  }
 
-
+  edit(client: Client){
+    this.formGroupClient.setValue(client)
+    this.currentClient = client;
+    this.isEditing = true
   }
 }
